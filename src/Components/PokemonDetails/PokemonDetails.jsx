65b8e993a9287b41085b972bef898a09a0b8849f
satyam <satyampayal10@gmail.com>
2024-05-19 23:26:29 +0530

    Finish Project

--- src/Components/PokemonDetails/PokemonDetails.jsx
+++ src/Components/PokemonDetails/PokemonDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import axios from 'axios'
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 function PokemonDetails() {
     const { id } = useParams();
@@ -25,24 +26,31 @@ function PokemonDetails() {
         downloadPokemon();
     })
     return (
-        
-        pokemon && <div className="  italic grid grid-cols-2 w-[100%] h-[100vh] justify-center place-content-center bg-[rgba(0,0,0,0.7)] text-white">
-            <div className="  justify-items-center max-w-[50%] ml-[200px] mt-[50px]" >
-                <h1 className="text-[24px]">Name:{pokemon.name}</h1>
+        <>
+            <h1 className="bg-pink-500 text-white font-[24px]  ">
+                <Link to="/" className="ml-[60px] inline-block border-[1px] border-white rounded-[5px]
+                 w-[80px] text-center px-[16px] py-[6px] bg-blue-400 m-2 hover:scale-[96%] hover:translate-y-[-2px]">BACK</Link>
+            </h1>
+            {pokemon && <div className="  italic grid grid-cols-2 w-[100%] h-[100vh] justify-center place-content-center bg-[rgba(0,0,0,0.7)] text-white">
+                <div className="  justify-items-center max-w-[50%] ml-[200px] mt-[50px]" >
+                    <h1 className="text-[24px]">Name:{pokemon.name}</h1>
 
-                <div className="text-blue-400 text-[20px] mt-[10px]">
-                   <p> Weight:{pokemon.weight}</p>
-                   <p> Height:{pokemon.height}</p>
+                    <div className="text-blue-400 text-[20px] mt-[10px] m-4 w-20 border-2">
+                        <p> Weight:{pokemon.weight}</p>
+                        <p> Height:{pokemon.height}</p>
+                    </div>
+
+                    <div className="mt-[20px]"> <span className="text-pink-400 text-[20px]">Types:</span>
+                        {pokemon.types.map(t => <span className="inline-block border-[1px] border-black rounded-[14px]   bg-pink-500 ml-[20px]  text-center px-[16px] py-[6px] w-[70px]" key={t.type.name}>{t.type.name}</span>)}</div>
+                </div>
+                <div>
+                    <img src={pokemon.image} alt="" />
                 </div>
 
-                <div className="mt-[20px]"> <span className="text-pink-400 text-[20px]">Types:</span>
-                {pokemon.types.map(t => <span className="inline-block border-[1px] border-black rounded-[14px]   bg-pink-500 ml-[20px]  text-center px-[16px] py-[6px] w-[70px]" key={t.type.name}>{t.type.name}</span>)}</div>
-            </div>
-            <div>
-                <img src={pokemon.image} alt="" />
-            </div>
+            </div>}
 
-        </div>
+        </>
+
     )
 }
 export default PokemonDetails;
--- src/Components/PokemonList/PokedexList.jsx
+++ src/Components/PokemonList/PokedexList.jsx
@@ -4,11 +4,11 @@ import { useEffect, useState } from 'react'
 import Pokemon from '../Pokemon/Pokemon';
 
 function PokedexList() {
-    const DEFAULT_URL="https://pokeapi.co/api/v2/pokemon";
-    const [pokedex_url,setPokedex_url] = useState("https://pokeapi.co/api/v2/pokemon");
+    const DEFAULT_URL = "https://pokeapi.co/api/v2/pokemon";
+    const [pokedex_url, setPokedex_url] = useState("https://pokeapi.co/api/v2/pokemon");
     const [pokemonList, setPokemonList] = useState([]);
-    const [nextUrl,setNextUrl]=useState(DEFAULT_URL);
-    const [prevUrl,setPrevUrl]=useState(DEFAULT_URL);
+    const [nextUrl, setNextUrl] = useState(DEFAULT_URL);
+    const [prevUrl, setPrevUrl] = useState(DEFAULT_URL);
     // console.log(url)
     async function downloadPokemons() {
         const response = await axios.get(pokedex_url ? pokedex_url : DEFAULT_URL);
@@ -30,7 +30,7 @@ function PokedexList() {
                 types: pokemon.types
             }
         });
-      //console.log(pokemonFinalList);
+        //console.log(pokemonFinalList);
         setPokemonList(pokemonFinalList);
     }
     useEffect(() => {
@@ -40,20 +40,20 @@ function PokedexList() {
 
     return (
         <div className='w[100%] h-[100%]'>
-                <h1 className='text-[24px] text-center m-3'>Pokemon List</h1>
-                <div className='mt-[20px] mb-[30px] w-[100%] h-3 flex gap-2 justify-center items-center'>
-                <button onClick={()=>setPokedex_url(prevUrl)} className='border-[1px] w-[60px] px-[16px] py-[6px] border-none bg-blue-500 text-white cursor-pointer hover:scale-[102%] '>prev</button>
-                <button onClick={()=>setPokedex_url(nextUrl)} className='border-[1px] w-[60px] px-[16px] py-[6px] border-none bg-green-500 text-white cursor-pointer hover:scale-[102%]'>Next</button>
+            <h1 className='text-[24px] text-center m-3'>Pokemon List</h1>
+            <div className='mt-[20px] mb-[30px] w-[100%] h-3 flex gap-2 justify-center items-center'>
+                <button onClick={() => setPokedex_url(prevUrl)} className='border-[1px] w-[60px] px-[16px] py-[6px] border-none bg-blue-500 text-white cursor-pointer hover:scale-[102%] '>prev</button>
+                <button onClick={() => setPokedex_url(nextUrl)} className='border-[1px] w-[60px] px-[16px] py-[6px] border-none bg-green-500 text-white cursor-pointer hover:scale-[102%]'>Next</button>
             </div>
             <div className='px-[20px] grid lg:grid-cols-3 md:grid-cols-3 grid-cols-2 gap-3'>
-                {pokemonList.map(pokemon=> <Pokemon name={pokemon.name} key={pokemon.id} id={pokemon.id} url={pokemon.image} type={pokemon.types}/>)}
+                {pokemonList.map(pokemon => <Pokemon name={pokemon.name} key={pokemon.id} id={pokemon.id} url={pokemon.image} type={pokemon.types} />)}
             </div>
             <div className='mt-[20px] mb-[30px] w-[100%] h-3 flex gap-2 justify-center items-center'>
-                <button onClick={()=>setPokedex_url(prevUrl)} className='border-[1px] w-[60px] px-[16px] py-[6px] border-none bg-blue-500 text-white cursor-pointer hover:scale-[102%] '>prev</button>
-                <button onClick={()=>setPokedex_url(nextUrl)} className='border-[1px] w-[60px] px-[16px] py-[6px] border-none bg-green-500 text-white cursor-pointer hover:scale-[102%]'>Next</button>
+                <button onClick={() => setPokedex_url(prevUrl)} className='border-[1px] w-[60px] px-[16px] py-[6px] border-none bg-blue-500 text-white cursor-pointer hover:scale-[102%] '>prev</button>
+                <button onClick={() => setPokedex_url(nextUrl)} className='border-[1px] w-[60px] px-[16px] py-[6px] border-none bg-green-500 text-white cursor-pointer hover:scale-[102%]'>Next</button>
             </div>
 
-            
+
         </div>
     )
 }
